cc8f1b2e448c9592b3eccebae222281dfe6065bf
xed-programmer <xedprogrammer@gmail.com>
2022-08-14 11:45:20 +0800

    asd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
             <div class="container">
                 <a href="{{ route('home') }}" class="navbar-brand">
                     <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                    <span class="brand-text font-weight-light">Pulse Monitoring</span>
+                    <span class="brand-text font-weight-light">BOOM Dev</span>
                   </a>
             
                   <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
